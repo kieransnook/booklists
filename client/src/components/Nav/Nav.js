506d0a79e9983b3550670253edd2b82fa852c083
Snookie <kieransnook35@gmail.com>
2020-05-08 19:20:29 -0400

    made rest of components

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,13 +1,14 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Nav() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
-  );
+    return(
+        <nav>
+            <h2>Google Books</h2>
+            <Link to="/">Search</Link>
+            <Link to="/saved">Saved</Link>
+        </nav>
+    );
 }
 
 export default Nav;
